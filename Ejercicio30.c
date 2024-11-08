9328d7154fcf13bb4220f6f8b81b2cd9967ffe1b
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-07 20:25:07 -0600

    Algoritmo que multiplica dos arreglos

--- Ejercicio30.c
+++ Ejercicio30.c
@@ -2,31 +2,38 @@
  * Fecha: 18-07-2023
  * Correo: orlandourbanotrejo@gmail.com
  *
- * Algoritmo que multiplique dos arreglos */
+ * Algoritmo que multiplica dos arreglos */
 
 #include <stdio.h>
+
 int main() {
-    int primero[5], segundo[5], multiplicacion[5], suma;
-	for(int i = 0; i < 5; i++){
-        // Entrada de datos
-		printf("Valor %d: \n",i+1);
+    int primero[5], segundo[5], multiplicacion[5];
+    
+    // Entrada de datos para el primer arreglo
+    printf("Ingresa los valores del primer arreglo:\n");
+    for(int i = 0; i < 5; i++){
+        printf("Valor %d: ", i + 1);
         scanf("%d", &primero[i]);
     }
-    puts("");
+
+    // Entrada de datos para el segundo arreglo
+    printf("\nIngresa los valores del segundo arreglo (en orden inverso):\n");
     for(int i = 4; i >= 0; i--){
-    	suma = i + 2;
-        printf("Valor %d: \n",suma-1);
+        printf("Valor %d: ", i + 1);  // Se ajustó el mensaje para mejorar la claridad.
         scanf("%d", &segundo[i]);
     }
-	for(int i = 0; i < 5; i++){
+
+    // Multiplicación de los arreglos
+    for(int i = 0; i < 5; i++){
         multiplicacion[i] = primero[i] * segundo[i];
     }
 
-    //Impresion de resultado
-	puts("Resultado: ");
+    // Impresión de resultados
+    printf("\nResultado de la multiplicación de los arreglos:\n");
     for(int i = 0; i < 5; i++){
-        printf("%d ", multiplicacion[i]);
+        printf("Elemento %d: %d\n", i + 1, multiplicacion[i]);
     }
-	 return 0;
+
+    return 0;
 }
 
