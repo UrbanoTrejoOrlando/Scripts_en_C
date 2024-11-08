337539247c009706c005df217360a107a08b4234
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-08 08:58:27 -0600

    Este programa calcula la suma de los primeros diez números pares (0, 2, 4, ..., 18).

--- Ejercicio37.c
+++ Ejercicio37.c
@@ -1,18 +1,21 @@
-/* Autor:  Orlando Urbano Trejo (Lando)
- * Fecha:  20-07-2023 
+/* Autor: Orlando Urbano Trejo (Lando)
+ * Fecha: 20-07-2023 
  * Correo: urbanorlando79@gmail.com
  *
- * Ejercicio: Haz un codigo que haga la suma de los primeros diez numeros pares */
+ * Ejercicio: Este programa calcula la suma de los primeros diez números pares (0, 2, 4, ..., 18). */
 
 #include <stdio.h>
 
-int main(){
-	 int Suma = 0;
-	 for(int i = 0; i <=20; i++){
-	 	 if(i % 2 == 0){
-		 	Suma += i;
-		 }
-	 }
-	 printf("Resultado: %d\n", Suma);
-	 return 0;
+int main() {
+    int suma = 0;
+
+    // Iteramos de 0 a 18 en pasos de 2, sumando cada número par
+    for (int i = 0; i < 20; i += 2) {
+        suma += i;
+    }
+
+    // Mostramos el resultado
+    printf("Resultado: %d\n", suma);
+    return 0;
 }
+
