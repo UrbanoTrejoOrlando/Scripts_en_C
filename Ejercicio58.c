a3129281dbfc9fe0fcbb26700e48e4a3643474bf
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-08 10:24:04 -0600

    Realiza un programa que lea una cadena por teclado y compruebe si es una letra mayúscula

--- Ejercicio58.c
+++ Ejercicio58.c
@@ -2,21 +2,22 @@
  * Fecha:  26-07-2023 
  * Correo: urbanorlando79@gmail.com
  *
- * Ejercicio: Realiza un programa que lea una cadena por teclado y compruebe si es una letra mayuscula */
+ * Ejercicio: Realiza un programa que lea una cadena por teclado y compruebe si es una letra mayúscula */
 
 #include <stdio.h>
-#include <string.h>
-// Metodo principal
-int main(){
-    char letra[5];
+
+// Método principal
+int main() {
+    char letra;
     puts("Ingresa una letra:");
-    scanf("%s",&letra);
-    if(strcmp(letra,"A")>=0 && strcmp(letra,"Z") <= 0){
-        printf("La letra %s es mayuscula\n",letra);
+    scanf(" %c", &letra);  // Usamos %c para capturar un solo carácter
+
+    if (letra >= 'A' && letra <= 'Z') {
+        printf("La letra %c es mayúscula\n", letra);
+    } else {
+        printf("La letra %c es minúscula\n", letra);
     }
-	 else{
-        printf("La letra %s es minuscula\n",letra);
-    }
+
     return 0;
-}// Fin del metodo
+} // Fin del método
 
