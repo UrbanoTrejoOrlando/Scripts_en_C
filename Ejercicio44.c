fffc46db611ec54098c31d1b081d14bdeb1149e2
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-08 09:08:48 -0600

    Hacer un menú que considere varias opciones

--- Ejercicio44.c
+++ Ejercicio44.c
@@ -1,44 +1,61 @@
-/* Autor:  Orlando Urbano Trejo (Lando)
- * Fecha:  25-07-2023 
+/* Autor: Orlando Urbano Trejo (Lando)
+ * Fecha: 25-07-2023 
  * Correo: urbanorlando79@gmail.com
  *
- * Ejercicio: Hacer un menu que considere las siguientes opciones:
- * 1) Cubo de un numero, 2) Numero par o impar, 3) Salir, */
+ * Ejercicio: Hacer un menú que considere las siguientes opciones:
+ * 1) Cubo de un número, 2) Número par o impar, 3) Salir
+ */
 
 #include <stdio.h>
 #include <math.h>
 
-int main(){
-	 int Opcion, Numero;
-	 float Cuadrado = 0, Cubo = 0;
-    puts("------MENU DE OPCIONES------");
-    puts("1) Cubo de un numero");
-    puts("2) Numero par o impar");
-    puts("3) Salir");
-	 puts("Elige una opcion");
-	 scanf("%d",&Opcion);
-	 if(Opcion == 1){
-	 	  puts("Ingresa un numero: ");
-		  scanf("%d",&Numero);
-		  Cubo = pow(Numero,3);
-		  printf("El cubo de %d es: %.2f\n",Numero, Cubo);
-	 }
-    else if(Opcion == 2){
-	     puts("Ingresa un numero: ");
-        scanf("%d",&Numero);
-		  if(Numero % 2 == 0){
-		      puts("Numero par");
-        }
-		  else{
-		      puts("Numero impar");
-		  }
-    }
-	 else if(Opcion == 3){
-		  puts("Fin del programa");
-	 }
-	 else{
-	     puts("Opcion invalida");
-	 }
+int main() {
+    int Opcion, Numero;
+    float Cubo = 0;
 
-	 return 0;
+    // Menú principal
+    do {
+        puts("------MENU DE OPCIONES------");
+        puts("1) Cubo de un numero");
+        puts("2) Numero par o impar");
+        puts("3) Salir");
+        puts("Elige una opcion:");
+
+        // Solicita la opción del usuario
+        scanf("%d", &Opcion);
+
+        switch (Opcion) {
+            case 1:
+                // Opción para calcular el cubo
+                puts("Ingresa un número: ");
+                scanf("%d", &Numero);
+                Cubo = pow(Numero, 3); // Calcula el cubo del número
+                printf("El cubo de %d es: %.2f\n", Numero, Cubo);
+                break;
+            
+            case 2:
+                // Opción para verificar si el número es par o impar
+                puts("Ingresa un número: ");
+                scanf("%d", &Numero);
+                if (Numero % 2 == 0) {
+                    puts("Número par");
+                } else {
+                    puts("Número impar");
+                }
+                break;
+
+            case 3:
+                // Opción para salir
+                puts("Fin del programa.");
+                break;
+
+            default:
+                // Opción inválida
+                puts("Opción inválida, intenta nuevamente.");
+                break;
+        }
+    } while (Opcion != 3); // El menú se repite hasta que el usuario elija salir
+
+    return 0;
 }
+
