1ee714911749f138fbf4971ac00a49867351f627
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-08 10:20:43 -0600

    Calcular el mayor de dos numeros leídos del teclado y visualizarlos en pantalla

--- Ejercicio55.c
+++ Ejercicio55.c
@@ -2,23 +2,29 @@
  * Fecha:  26-07-2023 
  * Correo: urbanorlando79@gmail.com
  *
- * Ejercicio: Calcular el mayor de dos numeros leidos del teclado y visualizarlos en pantalla. */
+ * Ejercicio: Calcular el mayor de dos numeros leídos del teclado y visualizarlos en pantalla. */
 
 #include <stdio.h>
-// Metodo principal
-int main(){
+
+// Método principal
+int main() {
     int Numero1, Numero2;
-    puts("Ingresa el numero 1:");
-    scanf("%d",&Numero1);
-    puts("Ingresa el numero 2:");
-    scanf("%d",&Numero2);
-    if(Numero1 > Numero2){
-        printf("El numero mayor es: %d\n",Numero1);
-    }else if(Numero1 == Numero2){
-	     puts("Los numeros son iguales");
-	 }
-	 else{
-        printf("El numero mayor es: %d\n",Numero2);
+
+    // Solicitar entrada de los dos números
+    puts("Ingresa el número 1:");
+    scanf("%d", &Numero1);
+    puts("Ingresa el número 2:");
+    scanf("%d", &Numero2);
+
+    // Comparar y mostrar el mayor o indicar si son iguales
+    if (Numero1 > Numero2) {
+        printf("El número mayor es: %d\n", Numero1);
+    } else if (Numero1 == Numero2) {
+        puts("Los números son iguales.");
+    } else {
+        printf("El número mayor es: %d\n", Numero2);
     }
+
+    return 0; // Finalizar el programa correctamente
 }
 
