9961b1a3941d9deda0e9ce4510a62190de806a09
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-08 09:16:52 -0600

    Define un array de 10 números enteros con nombre Num y asigna 5 valores

--- Ejercicio49.c
+++ Ejercicio49.c
@@ -1,15 +1,21 @@
-/* Autor:  Orlando Urbano Trejo (Lando)
- * Fecha:  25-07-2023 
+/* Autor: Orlando Urbano Trejo (Lando)
+ * Fecha: 25-07-2023 
  * Correo: urbanorlando79@gmail.com
  *
- * Ejercicio: Define un array de 10 numeros enteros con nombre Num y asigna 5 valores.Muestra el contenido de todos los elementos del array */
+ * Ejercicio: Define un array de 10 números enteros con nombre Num y asigna 5 valores.
+ * Muestra el contenido de todos los elementos del array.
+ */
 
 #include <stdio.h>
-int main(){
-	 int Num[12];
-  	 Num[0] = 1, Num[1] = -2, Num[4] = 0, Num[6] = 14, Num[8] = 5; 	 
-	 for(int i = 0; i < 12; i++){
-	     printf("Posicion %d: %d\n",i,Num[i]);
-	 }
-	 return 0;
+
+int main() {
+    int Num[10] = {1, -2, 0, 0, 14, 0, 5, 0, 0, 0};  // Inicializa el array con los valores dados
+
+    // Mostrar el contenido de todos los elementos del array
+    for (int i = 0; i < 10; i++) {
+        printf("Posición %d: %d\n", i, Num[i]);
+    }
+
+    return 0;
 }
+
