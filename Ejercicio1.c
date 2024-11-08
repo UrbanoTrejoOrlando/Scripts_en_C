4dec54bf9ce07d447ed931ec76c64fb3c372d68d
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-07 19:41:16 -0600

    Algoritmo que calcula la letra correspondiente a la calificación ingresada.

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -1,38 +1,41 @@
-/* Autor: Orlando Urbano Trejo @Lando
+/* 
+ * Autor: Orlando Urbano Trejo @Lando
  * Fecha: 11-07-2023
  * Correo: orlandourbanotrejo@gmail.com
  *
- * Algoritmo que calcula la letra que te corresponde dependiendo de la calificacion */
+ * Algoritmo que calcula la letra correspondiente a la calificación ingresada.
+ */
 
 #include <stdio.h>
-int main(){
-	int calificacion;
-	// Entrada de datos
-	puts("Ingresa la calificacion obtenida (0-10): ");
-	scanf("%d",&calificacion);
-	switch(calificacion){
-	    // Evaluaciones
-		case 0:
-		case 1:	
-		case 2:
-		case 3: 
-		case 4:
-		case 5: 
-			puts("Tu calificacion es: F");
-			break;
-		case 6: 
-		case 7:
-		    puts("Tu calificacion es: D");
-			break;
-		case 8:
-			puts("Tu calificacion es: C");
-			break;
-		case 9: 
-		    puts("Tu calificacion es: B");
-			break;
-		case 10:
-			puts("Tu calificacion es: A");
-			break;
-	}
-   return 0;
+
+int main() {
+    int calificacion;
+
+    // Entrada de datos
+    printf("Ingresa la calificación obtenida (0-10): ");
+    scanf("%d", &calificacion);
+
+    // Evaluación de la calificación
+    switch(calificacion) {
+        case 0: case 1: case 2: case 3: case 4: case 5:
+            printf("Tu calificación es: F\n");
+            break;
+        case 6: case 7:
+            printf("Tu calificación es: D\n");
+            break;
+        case 8:
+            printf("Tu calificación es: C\n");
+            break;
+        case 9:
+            printf("Tu calificación es: B\n");
+            break;
+        case 10:
+            printf("Tu calificación es: A\n");
+            break;
+        default:
+            printf("Calificación inválida. Debe estar en el rango de 0 a 10.\n");
+    }
+
+    return 0;
 }
+
