67709a29512760af4b14843bde9657746b8360ff
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-08 09:05:29 -0600

    Este programa calcula la suma de la serie (1-2+3-4+5-6+... hasta n)

--- Ejercicio41.c
+++ Ejercicio41.c
@@ -1,24 +1,31 @@
-/* Autor:  Orlando Urbano Trejo (Lando)
- * Fecha:  22-07-2023 
+/* Autor: Orlando Urbano Trejo (Lando)
+ * Fecha: 22-07-2023 
  * Correo: urbanorlando79@gmail.com
  *
- * Ejercicio: Realiza un codigo que calcule la siguiente suma (1-2+3-4+5-6 hasta n) donde n sea ingresada por el usuario */ 
+ * Ejercicio: Este programa calcula la suma de la serie (1-2+3-4+5-6+... hasta n),
+ * donde n es un número ingresado por el usuario. */
 
 #include <stdio.h>
-int main(){
-	 int Numero, Suma = 0, Negativos = 0, Pares = 0, Impares = 0;
-	 puts("Ingresa un numero");
-	 scanf("%d",&Numero);
-	 for(int i = 1; i <= Numero; i++){
-	 	  if(i % 2 == 0){
-		  		Negativos = i * (-1);
-				Pares += Negativos;
-		  }
-		  else{
-		      Impares += i;
-		  }
-	 }
-	 Suma = Pares + Impares;
-	 printf("Suma: %d\n", Suma);
-	 return 0;
+
+int main() {
+    int numero, suma = 0;
+
+    // Solicita al usuario el valor de 'n'
+    puts("Ingresa un número:");
+    scanf("%d", &numero);
+
+    // Realiza la suma de los números alternando entre positivos y negativos
+    for (int i = 1; i <= numero; i++) {
+        if (i % 2 == 0) {
+            suma -= i; // Resta los números pares
+        } else {
+            suma += i; // Suma los números impares
+        }
+    }
+
+    // Muestra el resultado
+    printf("Suma: %d\n", suma);
+
+    return 0;
 }
+
