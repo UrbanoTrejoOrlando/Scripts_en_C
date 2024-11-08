c0e0250b6f76b0fd82fafc993b350d44e8d33a4e
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-07 20:19:53 -0600

    Algoritmo que calcula la calificacion de N alumnos y obtiene el promedio general

--- Ejercicio26.c
+++ Ejercicio26.c
@@ -1,23 +1,41 @@
+#include <stdio.h>
+
 /* Orlando Urbano Trejo @Lando
  * Fecha: 17-07-2023
  * Correo: orlandourbanotrejo@gmail.com
  *
- * Algoritmo que calcula la calificacion de N alumnos y obtiene el promedio general.*/
+ * Algoritmo que calcula la calificacion de N alumnos y obtiene el promedio general. */
 
-#include <stdio.h>
+// Prototipo de la función para calcular el promedio de edades
+float calcularPromedioEdades(int alumno);
+
 int main(){
- 	int alumno, edad;
-    float suma = 0, promedio = 0;
-	// Entrada de datos
+    int alumno;
+    
+    // Entrada de datos
     puts("Cuantos alumnos hay en el salon: ");
-    scanf("%d",&alumno);
+    scanf("%d", &alumno);
+    
+    // Calcular el promedio de edades
+    float promedio = calcularPromedioEdades(alumno);
+    
+    // Impresión de resultados
+    printf("El promedio de las edades de los alumnos es: %.2f\n", promedio);
+    
+    return 0;
+}
+
+// Función para calcular el promedio de las edades de los alumnos
+float calcularPromedioEdades(int alumno) {
+    int edad;
+    float suma = 0;
+    
     for(int i = 0; i < alumno; i++){
-    	printf("Edad del alumno %d: \n",i+1);
-        scanf("%d",&edad);
+        printf("Edad del alumno %d: \n", i + 1);
+        scanf("%d", &edad);
         suma += edad;
     }
-    promedio = suma / alumno;
-    printf("El promedio de las edades de los alumnos es: %.2f\n",promedio);
-    return 0;
+    
+    return suma / alumno;
 }
 
