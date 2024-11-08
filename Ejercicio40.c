0f2568e3d01ba6b3911a37710b932ab56c9547c3
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-08 09:03:43 -0600

    Este programa imprime los números de 5 en 5 hasta el límite que el usuario indique

--- Ejercicio40.c
+++ Ejercicio40.c
@@ -1,19 +1,24 @@
-/* Autor:  Orlando Urbano Trejo (Lando)
- * Fecha:  22-07-2023 
+/* Autor: Orlando Urbano Trejo (Lando)
+ * Fecha: 22-07-2023 
  * Correo: urbanorlando79@gmail.com
  *
- * Ejercicio: Realiza un codigo que imprima los numeros de 5 en 5 hasta donde el usuario digite */
+ * Ejercicio: Este programa imprime los números de 5 en 5 hasta el límite que el usuario indique. */
 
 #include <stdio.h>
 
-int main(){
-	int Numero;
-  	puts("Ingresa un numero: ");
-	scanf("%d",&Numero);
-	for(int i = 1; i <= Numero; i++){
-		 if(i % 5 == 0){
-		 	 printf("%d, ",i);
-		 }
-	}  	
-   return 0;
+int main() {
+    int numero;
+
+    // Solicita al usuario el límite de la secuencia
+    puts("Ingresa un número límite: ");
+    scanf("%d", &numero);
+
+    // Imprime los números de 5 en 5 hasta el límite
+    for (int i = 5; i <= numero; i += 5) {
+        printf("%d, ", i);
+    }
+
+    printf("\n");  // Nueva línea al final de la secuencia
+    return 0;
 }
+
