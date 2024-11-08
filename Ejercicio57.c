0749fdad3e9f6a22a016ee1b11a57a8022357a98
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-08 10:23:00 -0600

    Ingresa por teclado el nombre y el signo de cualquier persona e imprime el nombre solo si la persona es signo Aries; caso contrario,

--- Ejercicio57.c
+++ Ejercicio57.c
@@ -2,23 +2,27 @@
  * Fecha:  26-07-2023 
  * Correo: urbanorlando79@gmail.com
  *
- * Ejercicio: Ingresa por teclado el nombre y el signo de cualquier persona e imprima el nombre solo si la persona es signo Aries, caso contrario imprimir "No es signo Aries" */
+ * Ejercicio: Ingresa por teclado el nombre y el signo de cualquier persona e imprime el nombre solo si la persona es signo Aries; caso contrario, imprime "No es signo Aries". */
 
 #include <stdio.h>
 #include <string.h>
-// Metodo principal
-int main(){
+
+// Método principal
+int main() {
     char Nombre[30];
     char Signo[20];
+
     puts("Ingresa tu nombre:");
-    scanf("%s",&Nombre);
+    scanf("%s", Nombre);  // No es necesario el & al ser una cadena
     puts("Ingresa tu signo:");
-    scanf("%s",&Signo);
-    if(strcmp(Signo,"Aries")==0){
-        printf("Eres signo aries, tu nombre es: %s\n",Nombre);
-    } 
-	 else{
-        puts("No eres signo aries");
-        }
+    scanf("%s", Signo);
+
+    if (strcmp(Signo, "Aries") == 0) {
+        printf("Eres signo Aries, tu nombre es: %s\n", Nombre);
+    } else {
+        puts("No eres signo Aries");
+    }
+
     return 0;
-}// Fin del metodo
+} // Fin del método
+
