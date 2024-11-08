f732f278c4e90e46d32382f7e4d700f5199f2508
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-07 20:22:42 -0600

    Algoritmo que imprime un árbol de Navidad

--- Ejercicio28.c
+++ Ejercicio28.c
@@ -1,40 +1,61 @@
+#include <stdio.h>
+
 /* Orlando Urbano Trejo @Lando
  * Fecha: 17-07-2023
  * Correo: orlandourbanotrejo@gmail.com
  *
- * Algoritmo que imprima un arbol de navidad */
+ * Algoritmo que imprime un árbol de Navidad */
 
-#include <stdio.h>
+// Prototipos de funciones
+void imprimirArbol(int altura);
+void imprimirTronco(int altura);
+
 int main(){
-	int altura, espacios, tronco;
+    int altura;
+    
     // Entrada de datos
-	puts("Altura del arbol: ");
+    puts("Altura del arbol: ");
     scanf("%d", &altura);
 
+    // Imprimir el árbol
+    imprimirArbol(altura);
+    // Imprimir el tronco del árbol
+    imprimirTronco(altura);
+
+    return 0;
+}
+
+// Función que imprime la parte principal del árbol
+void imprimirArbol(int altura){
+    int espacios;
+    
     for(int i = 1; i <= altura; i++){
         espacios = altura - i; 
-
-     	// Imprimir espacios en blanco
+        
+        // Imprimir espacios en blanco
         for(int j = 1; j <= espacios; j++){
             printf(" ");
         }
 
+        // Imprimir las estrellas
         for(int j = 1; j <= (2 * i - 1); j++) {
             printf("*");
         }
 
         printf("\n");
-    }  
-	tronco = altura - 1; 
+    }
+}
+
+// Función que imprime el tronco del árbol
+void imprimirTronco(int altura){
+    int tronco = altura - 1;
+
     // Imprimir el tronco del árbol
     for(int i = 1; i <= 2; i++){
         for(int j = 1; j <= tronco; j++){
-           	printf(" ");
+            printf(" ");
         }
         printf("**\n");
-    }  
-
-    return 0; 
+    }
 }
-
 
