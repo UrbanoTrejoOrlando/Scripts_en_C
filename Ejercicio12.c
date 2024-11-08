7acdce3c4b9ce7fea5cad173289e177d398444f3
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-07 20:05:41 -0600

    Algoritmo que determina los intereses que tiene una persona con el banco

--- Ejercicio12.c
+++ Ejercicio12.c
@@ -1,21 +1,36 @@
+#include <stdio.h>
+
 /* Orlando Urbano Trejo @Lando
  * Fecha: 12-07-2023
  * Correo: orlandourbanotrejo@gmail.com
  *
  * Algoritmo que determina los intereses que tiene una persona con el banco */
-#include <stdio.h>
-int main(){
-	int tiempo, tiempoTranscurrido;
-	float prestamo = 10000, interes = 0.27;
-	// Entrada de datos
-	puts("¿Anio en que solicito el prestamo?: ");
-	scanf("%d",&tiempo);
-	puts("Anio actual: ");
-	scanf("%d",&tiempoTranscurrido);
 
-	for(int i = tiempo; i <= tiempoTranscurrido; i++){
-		prestamo += + (prestamo * interes);
-		printf("El interes de %d es de: $%.2f\n",i,prestamo);
-	 }
+ // Prototipo de la función para calcular el interés
+void calcularIntereses(int tiempo, int tiempoTranscurrido, float* prestamo, float interes);
+
+int main(){
+    int tiempo, tiempoTranscurrido;
+    float prestamo = 10000, interes = 0.27;
+    
+    // Entrada de datos
+    puts("¿Anio en que solicito el prestamo?: ");
+    scanf("%d", &tiempo);
+    
+    puts("Anio actual: ");
+    scanf("%d", &tiempoTranscurrido);
+
+    // Calcular e imprimir los intereses para cada año
+    calcularIntereses(tiempo, tiempoTranscurrido, &prestamo, interes);
+    
     return 0;
 }
+
+// Función para calcular los intereses anuales
+void calcularIntereses(int tiempo, int tiempoTranscurrido, float* prestamo, float interes) {
+    for (int i = tiempo; i <= tiempoTranscurrido; i++) {
+        *prestamo += (*prestamo * interes); // Actualiza el prestamo con el interés
+        printf("El interes de %d es de: $%.2f\n", i, *prestamo);
+    }
+}
+
