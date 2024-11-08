82be038651fc4068b2d3f172173bf054cad4a184
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-08 09:06:20 -0600

    Convertir de grados Celsius a Fahrenheit

--- Ejercicio42.c
+++ Ejercicio42.c
@@ -1,15 +1,24 @@
-/* Autor:  Orlando Urbano Trejo (Lando)
- * Fecha:  23-07-2023 
+/* Autor: Orlando Urbano Trejo (Lando)
+ * Fecha: 23-07-2023 
  * Correo: urbanorlando79@gmail.com
  *
- * Ejercicio: Convertir de grados Celcius a Fahrenheit */
+ * Ejercicio: Convertir de grados Celsius a Fahrenheit */
+
 #include <stdio.h>
 
-int main(){
-    float Grados, Farenheit, Conversion = 0;
-	 puts("Ingresa los grados a convertir: ");
-	 scanf("%f",Grados);
-	 Conversion = (Grados * 1.8)+ 32;
-	 printf("Conversion: %.2f\n",Conversion);
-	 return 0;
+int main() {
+    float grados, fahrenheit;
+
+    // Solicita al usuario la temperatura en grados Celsius
+    puts("Ingresa los grados a convertir: ");
+    scanf("%f", &grados);  // Se debe pasar la dirección de memoria de 'grados' usando el operador '&'
+
+    // Realiza la conversión de Celsius a Fahrenheit
+    fahrenheit = (grados * 1.8) + 32;
+
+    // Muestra el resultado de la conversión
+    printf("Conversión: %.2f °F\n", fahrenheit);
+
+    return 0;
 }
+
