fb8ca064dd87297f160eb0121da5ab303bd06a45
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-08 10:25:09 -0600

    Realiza un programa que calcule la aceptación de una solicitud

--- Ejercicio59.c
+++ Ejercicio59.c
@@ -2,34 +2,38 @@
  * Fecha:  26-07-2023 
  * Correo: urbanorlando79@gmail.com
  *
- * Ejercicio: Realiza un programa que calcule la aceptacion de una solicitud en base a los siguientes parametros: edad, nota y sexo.
-* Minimo: Nota (5), edad (18), sexo M -> POSIBLE
-* Minimo: Nota (5), edad (18), sexo F -> ACEPTADA
-* Otros casos -> NO ACEPTADA */
+ * Ejercicio: Realiza un programa que calcule la aceptación de una solicitud en base a los siguientes parámetros: edad, nota y sexo.
+ * Mínimo: Nota (5), edad (18), sexo M -> POSIBLE
+ * Mínimo: Nota (5), edad (18), sexo F -> ACEPTADA
+ * Otros casos -> NO ACEPTADA */
 
 #include <stdio.h>
-#include <string.h>
-// Metodo principal
-int main(){
-    char Nota[5];
-    char Edad[5];
-    char Sexo[5];
-    puts("Ingresa tu calificacion obtenida:");
-    scanf("%s",&Nota);
+
+// Método principal
+int main() {
+    int Nota;
+    int Edad;
+    char Sexo;
+
+    puts("Ingresa tu calificación obtenida:");
+    scanf("%d", &Nota);
     puts("Ingresa tu edad:");
-    scanf("%s",&Edad);
-    puts("Ingresa sexo:");
-    scanf("%s",&Sexo);
-    if(strcmp(Nota,"5") == 0 && strcmp(Edad,"18") == 0 && strcmp(Sexo,"M") == 0){
-        printf("Solicitud (%s), Edad (%s), sexo (%s) ---> SOLICITUD POSIBLE\n",Nota,Edad,Sexo);
-    }
-	 else if(strcmp(Nota,"5") == 0 && strcmp(Edad,"18") == 0 && strcmp(Sexo,"F") == 0){
-        printf("Solicitud (%s), Edad (%s), sexo (%s) ---> SOLICITUD ACEPTADA\n",Nota,Edad,Sexo);
-    }
-	 else{
+    scanf("%d", &Edad);
+    puts("Ingresa tu sexo (M/F):");
+    scanf(" %c", &Sexo);
+
+    if (Nota >= 5 && Edad >= 18) {
+        if (Sexo == 'M') {
+            printf("Solicitud (Nota: %d), Edad (%d), Sexo (%c) ---> SOLICITUD POSIBLE\n", Nota, Edad, Sexo);
+        } else if (Sexo == 'F') {
+            printf("Solicitud (Nota: %d), Edad (%d), Sexo (%c) ---> SOLICITUD ACEPTADA\n", Nota, Edad, Sexo);
+        } else {
+            puts("Sexo no válido. SOLICITUD NO ACEPTADA");
+        }
+    } else {
         puts("SOLICITUD NO ACEPTADA");
     }
-    return 0;
-}// Fin del metodo
 
+    return 0;
+} // Fin del método
 
