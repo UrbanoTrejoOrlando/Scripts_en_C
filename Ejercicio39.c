ab0ffd3537ffdde8376703c52937900050de4c9c
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-08 09:02:28 -0600

    Este programa genera tablas de multiplicar de acuerdo al número de filas y columnas que el usuario introduzca.

--- Ejercicio39.c
+++ Ejercicio39.c
@@ -1,22 +1,28 @@
-/* Autor:  Orlando Urbano Trejo (Lando)
- * Fecha:  22-07-2023 
+/* Autor: Orlando Urbano Trejo (Lando)
+ * Fecha: 22-07-2023 
  * Correo: urbanorlando79@gmail.com
  *
- * Ejercicio: Realiza las tablas de multiplicar */
+ * Ejercicio: Este programa genera tablas de multiplicar de acuerdo al número de filas y columnas que el usuario introduzca. */
 
 #include <stdio.h>
-int main(){
-    int Filas, Columnas;
-	 puts("Numero de filas: ");
-	 scanf("%d",&Filas);
-	 puts("Numero de columnas: ");
-	 scanf("%d",&Columnas);
-	 for(int i = 1; i <= Filas; i++ ){
-		  printf("\n");
-	     for(int j = 1; j <= Columnas; j++){
-		  		printf("%d\t",i*j);
-		  }
-	 }
-	 return 0;
+
+int main() {
+    int filas, columnas;
+
+    // Solicita al usuario el número de filas y columnas para la tabla de multiplicar
+    puts("Introduce el número de filas: ");
+    scanf("%d", &filas);
+    puts("Introduce el número de columnas: ");
+    scanf("%d", &columnas);
+
+    // Genera las tablas de multiplicar
+    for (int i = 1; i <= filas; i++) {
+        for (int j = 1; j <= columnas; j++) {
+            printf("%d\t", i * j); // Imprime el producto de i y j en formato tabular
+        }
+        printf("\n");  // Nueva línea al final de cada fila
+    }
+
+    return 0;
 }
 
