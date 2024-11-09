ee18d7afbd8739a5986d3acea970c352abac9423
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-09 15:49:05 -0600

    Programa que determina si un año es bisiesto o no

--- Ejercicio61.c
+++ Ejercicio61.c
@@ -1,22 +1,30 @@
-/* Autor:  Orlando Urbano Trejo (Lando)
+/*
+ * Autor:  Orlando Urbano Trejo (Lando)
  * Fecha:  26-07-2023
  * Correo: urbanorlando79@gmail.com
  *
- * Ejercicio: Escribe un programa que reciba un año y nos diga si es bisiesto o no */
+ * Descripción: Programa que determina si un año es bisiesto o no.
+ * Un año es bisiesto si es divisible por 4, pero no si es divisible por 100,
+ * excepto cuando también es divisible por 400.
+ */
 
 #include <stdio.h>
-// Metodo principal
-int main(){
-    int Anio;
-    puts("Ingresa un año :");
-    scanf("%d",&Anio);
-    if(Anio%4 == 0){
-        puts("ES BISIESTO");
-    }
-	 else{
-        puts("ES ANIO NORMAL");
-    }  
-    return 0; 
-}// Fin del metodo
 
+// Función principal
+int main() {
+    int anio;
+
+    // Solicitar al usuario que ingrese un año
+    printf("Ingresa un año: ");
+    scanf("%d", &anio);
+
+    // Determinación de si el año es bisiesto
+    if ((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0)) {
+        printf("ES BISIESTO\n");
+    } else {
+        printf("ES AÑO NORMAL\n");
+    }
+
+    return 0;
+} // Fin de la función principal
 
