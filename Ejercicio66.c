ca310d23296b2998eceda1a591b9d5046144c105
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-09 15:56:06 -0600

    Programa en C que calcula la suma de los números pares desde un número inicial (n) hasta un número final (m)

--- Ejercicio66.c
+++ Ejercicio66.c
@@ -1,22 +1,34 @@
-/* Autor:  Orlando Urbano Trejo (Lando)
+/*
+ * Autor:  Orlando Urbano Trejo (Lando)
  * Fecha:  26-07-2023
  * Correo: urbanorlando79@gmail.com
  *
- * Ejercicio: Sumar numeros pares desde n hasta m. */
+ * Descripción: Programa en C que calcula la suma de los números pares desde un número inicial (n) hasta un número final (m).
+ */
 
 #include <stdio.h>
-int main(){
-    int Num_Inicio,Num_Final, Contador = 1, Suma = 0;
-    puts("Ingresa un numero para el inicio de la suma");
-    scanf("%d",&Num_Inicio);
-    puts("Ingresa un numero para el limite de la suma");
-    scanf("%d",&Num_Final);
-    for(int i = 1; i >= Num_Inicio && i <= Num_Final; i++){
-            if(i % 2 == 0){
-            Suma += i;
-            }
+
+// Función principal
+int main() {
+    int numInicio, numFinal, suma = 0;
+
+    // Solicitar al usuario que ingrese el número inicial y final
+    printf("Ingresa un número para el inicio de la suma: ");
+    scanf("%d", &numInicio);
+
+    printf("Ingresa un número para el límite de la suma: ");
+    scanf("%d", &numFinal);
+
+    // Calcular la suma de los números pares entre numInicio y numFinal
+    for (int i = numInicio; i <= numFinal; i++) {
+        if (i % 2 == 0) { // Verifica si el número es par
+            suma += i;
+        }
     }
-    printf("Suma: %d\n",Suma);
+
+    // Mostrar el resultado de la suma
+    printf("Suma: %d\n", suma);
+
     return 0;
-}// Fin del metodo
+} // Fin de la función principal
 
