00e44dcdce14d6195d410d30190e9e6fa0ee4f32
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-09 15:54:14 -0600

    Horas, Minutos y Segundos

--- Ejercicio64.c
+++ Ejercicio64.c
@@ -1,43 +1,38 @@
-/* Autor:  Orlando Urbano Trejo (Lando)
+/*
+ * Autor:  Orlando Urbano Trejo (Lando)
  * Fecha:  26-07-2023
  * Correo: urbanorlando79@gmail.com
  *
- * Ejercicio: Escribir en lenguaje C un programa que:
-1) Pida por teclado una hora en tres variables: horas, minutos y segundos (datos enteros).
-2) Muestre por pantalla:
-"HORA CORRECTA", en el caso de que la hora sea valida.
-"HORA INCORRECTA", en el caso de que la hora no sea valida.
-Nota: para que una hora sea valida, se tiene que cumplir que:
-    Las horas deben ser mayor o igual que 0 y menor o igual que 23.
-    Los minutos deben ser mayor o igual que 0 y menor o igual que 59.
-    Los segundos deben ser mayor o igual que 0 y menor o igual que 59. */
+ * Descripción: Programa en C que solicita una hora (horas, minutos y segundos) y verifica si es válida.
+ *
+ * Notas:
+ * - Las horas deben estar entre 0 y 23.
+ * - Los minutos y segundos deben estar entre 0 y 59.
+ */
 
 #include <stdio.h>
-// Metodo principal
-int main(){
-    int Horas, Minutos, Segundos;
-    puts("Ingresa las horas:");
-    scanf("%d",&Horas);
-    puts("Ingresa los minutos:");
-    scanf("%d",&Minutos);
-    puts("Ingresa los segundos:");
-    scanf("%d",&Segundos);
-    if(Horas >= 0 && Horas <=23 ){
-        if(Minutos >= 0 && Minutos <= 59){
-            if(Segundos >= 0 && Segundos <= 59){
-                puts("HORA CORRECTA");
-            }
-            else{
-                puts("HORA INCORRECTA");
-            }
-        }
-        else{
-            puts("HORA INCORRECTA");
-        }
+
+// Función principal
+int main() {
+    int horas, minutos, segundos;
+
+    // Solicitar al usuario que ingrese la hora, los minutos y los segundos
+    printf("Ingresa las horas (0-23): ");
+    scanf("%d", &horas);
+
+    printf("Ingresa los minutos (0-59): ");
+    scanf("%d", &minutos);
+
+    printf("Ingresa los segundos (0-59): ");
+    scanf("%d", &segundos);
+
+    // Verificar si la hora es válida
+    if (horas >= 0 && horas <= 23 && minutos >= 0 && minutos <= 59 && segundos >= 0 && segundos <= 59) {
+        printf("HORA CORRECTA\n");
+    } else {
+        printf("HORA INCORRECTA\n");
     }
-    else{
-        puts("HORA INCORRECTA");
-    }
+
     return 0;
-}// Fin del metodo
+} // Fin de la función principal
 
