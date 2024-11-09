42325879ed8219343ab62ec53bb69fb827ba5b1f
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-09 15:50:28 -0600

    Programa que recibe como entrada el número obtenido al lanzar un dado de seis caras

--- Ejercicio62.c
+++ Ejercicio62.c
@@ -1,39 +1,50 @@
-/* Autor:  Orlando Urbano Trejo (Lando)
+/*
+ * Autor:  Orlando Urbano Trejo (Lando)
  * Fecha:  26-07-2023
  * Correo: urbanorlando79@gmail.com
  *
- * Ejercicio:Realiza un programa que pida por teclado el resultado (dato entero) obtenido al lanzar un dado de seis caras y muestre por pantalla el numero en letras (dato cadena) de la cara opuesta al resultado obtenido.
-Nota 1: En las caras opuestas de un dado de seis caras estan los numeros: 1-6, 2-5 y 3-4.
-Nota 2: Si el numero del dado introducido es menor que 1 o mayor que 6, se mostrara el mensaje: ERROR: numero incorrecto.*/
+ * Descripción: Programa que recibe como entrada el número obtenido al lanzar un dado de seis caras
+ * (número entero) y muestra por pantalla el número en letras de la cara opuesta al resultado.
+ *
+ * Notas:
+ * - En un dado de seis caras, las caras opuestas son: 1-6, 2-5 y 3-4.
+ * - Si el número ingresado es menor que 1 o mayor que 6, se muestra un mensaje de error.
+ */
+
 #include <stdio.h>
-#include <string.h>
-// Metodo principal
-int main(){
-    int Cara;
-    puts("Ingresa la cara del dado:");
-    scanf("%d",&Cara);
-    switch(Cara){
+
+// Función principal
+int main() {
+    int cara;
+
+    // Solicitar al usuario el número de la cara del dado
+    printf("Ingresa la cara del dado (1 a 6): ");
+    scanf("%d", &cara);
+
+    // Determinación de la cara opuesta usando una estructura switch
+    switch (cara) {
         case 1:
-        puts("El valor contrario de la cara es: SEIS");
-        break;
+            printf("El valor contrario de la cara es: SEIS\n");
+            break;
         case 2:
-        puts("El valor contrario de la cara es: CINCO");
-        break;
+            printf("El valor contrario de la cara es: CINCO\n");
+            break;
         case 3:
-        puts("El valor contrario de la cara es: CUATRO");
-        break;
+            printf("El valor contrario de la cara es: CUATRO\n");
+            break;
         case 4:
-        puts("El valor contrario de la cara es: TRES");
-        break;
+            printf("El valor contrario de la cara es: TRES\n");
+            break;
         case 5:
-        puts("El valor contrario de la cara es: DOS");
-        break;
+            printf("El valor contrario de la cara es: DOS\n");
+            break;
         case 6:
-        puts("El valor contrario de la cara es: UNO");
-        break;
+            printf("El valor contrario de la cara es: UNO\n");
+            break;
         default:
-        puts("ERROR: Numero incorrecto");
+            printf("ERROR: Número incorrecto\n");
     }
-	 return 0;
-}
+
+    return 0;
+} // Fin de la función principal
 
