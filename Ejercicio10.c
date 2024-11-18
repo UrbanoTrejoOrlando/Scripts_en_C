4ee3e6555dfe26072fac802aec357c68cb8f321c
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-17 20:09:35 -0600

    Saldo actual de un cliente

--- Ejercicio10.c
+++ Ejercicio10.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
 // Prototipos de funciones
 void obtenerDatosCliente(char nombre[], float* saldoAnterior, float* deposito, float* montoCompras, float* saldoActual);
+void calcularYMostrarDatos(char nombre[], float saldoActual);
 
