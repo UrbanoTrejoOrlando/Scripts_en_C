51f39bc9d105010364ca4f1293c51b7f7f2a5ad6
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-20 11:56:26 -0600

    Algoritmo que determina los intereses que tiene una persona con el banc

--- Ejercicio12.c
+++ Ejercicio12.c
@@ -6,4 +6,5 @@
  * Algoritmo que determina los intereses que tiene una persona con el banco */
 
  // Prototipo de la función para calcular el interés
+void calcularIntereses(int tiempo, int tiempoTranscurrido, float* prestamo, float interes);
 
