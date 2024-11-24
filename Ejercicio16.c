275b89e9d7e7f4a67d9a0cd1b5b2bea2b0172d65
WolverinLando <orlandourbanotrejo@gmail.com>
2024-11-24 01:08:44 -0600

    ALgoritmo que calcula el presupuesto anual en tres areas de un hospital

--- Ejercicio16.c
+++ Ejercicio16.c
@@ -22,4 +22,5 @@ int main(){
     // Mostrar los resultados
     printf("Presupuesto Urgencias: $%.2f\n", urgencias);
     printf("Presupuesto Pediatria: $%.2f\n", pediatria);
+    printf("Presupuesto Traumatologia: $%.2f\n", traumatologia);
 
